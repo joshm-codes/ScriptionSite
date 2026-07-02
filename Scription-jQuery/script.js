5c882b5af8278b76f865181916b41a00bf561a44
Josh <Randspear1979@gmail.com>
2026-07-02 18:49:53 -0400

    Implement Flexslider with working autoslide

---
+++ Scription-jQuery/script.js
@@ -0,0 +1,26 @@
+(function (){
+    $(window).on('load', function() {
+        let restartTimer;
+
+        $('.flexslider').flexslider({
+            animation: 'slide',
+            slideshow: true,
+            slideshowSpeed: 4000,
+            animationSpeed: 600,
+            pauseOnAction: true,  
+            pauseOnHover: false,
+
+            start: function(slider) {
+                clearTimeout(restartTimer);
+            },
+
+            after: function(slider) {
+                clearTimeout(restartTimer);
+
+                restartTimer = setTimeout(function() {
+                    slider.play();   
+                }, 6000); 
+            }
+        });
+    });
+})();
